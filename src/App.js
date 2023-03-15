89eb11c846d8df0c878e3428adf83037fee47898
2zzzyoung <mzooyoungn@gmail.com>
2023-03-16 01:03:10 +0900

    outlet / params

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Videos from "./pages/Videos";
+import Root from "./pages/Root";
+import VideoDetail from "./pages/VideoDetail";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <p>Home</p>,
-    errorElement: <p>Not Found😅</p>,
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/videos", element: <Videos /> },
+      { path: "/videos/:videoId", element: <VideoDetail /> },
+    ],
   },
   {
     path: "/videos",
-    element: <p>Videos</p>,
+    element: <Videos />,
   },
 ]);
 
